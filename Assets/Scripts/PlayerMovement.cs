a03b1c692d39e50a658a2f42669f20eed4b14b63
Greyson Shafiei <gshafiei@charlotte.edu>
2025-02-19 17:32:12 -0500

    PlayerMovement Initialization

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,4 +5,14 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D playerBody;
+
+    private void Awake()
+    {
+        playerBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        
+    }
 }
