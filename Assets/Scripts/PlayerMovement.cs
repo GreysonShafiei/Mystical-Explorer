3ac026c995b00a1110b3160cb63e8b4927354f60
Greyson Shafiei <gshafiei@charlotte.edu>
2025-03-03 21:56:45 -0500

    Movement Animations Created

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,20 +5,62 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private LayerMask groundLayer;
     private Rigidbody2D playerBody;
+    private Animator animator;
+    private BoxCollider2D boxCollider;
 
     private void Awake()
-    {
+    {   
+        //Get references to character features
         playerBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     private void Update()
     {
+        float horizontalInpt = Input.GetAxis("Horizontal");
         playerBody.velocity = new Vector2(Input.GetAxis("Horizontal")* speed,playerBody.velocity.y);
 
-        if (Input.GetKey(KeyCode.Space))
+
+        //Player flipping when going left or right
+        if (horizontalInpt > 0.01f) 
         {
-            playerBody.velocity = new Vector2(playerBody.velocity.x, speed);
+            transform.localScale = Vector3.one;
         }
+        else if (horizontalInpt < -0.01f)
+        {
+            transform.localScale = new Vector3(-1,1,1);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow) && isGrounded())
+        {
+            jump();
+        }
+
+        //Setter for animator parameters
+        animator.SetBool("Run", horizontalInpt != 0);
+        animator.SetBool("grounded", isGrounded());
+    }
+
+    private void jump()
+    {
+        playerBody.velocity = new Vector2(playerBody.velocity.x, speed);
+        animator.SetTrigger("jump");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            
+        }
+    }
+
+    private bool isGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0 , Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
     }
 }
