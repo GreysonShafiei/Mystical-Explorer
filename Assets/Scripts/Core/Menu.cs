8c276c671996c9155a46de116f7fb616aa4036ef
Greyson Shafiei <gshafiei@charlotte.edu>
2025-04-02 22:46:45 -0400

    feat: add multiple menus

--- Assets/Scripts/Core/Menu.cs
+++ Assets/Scripts/Core/Menu.cs
@@ -5,10 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    string currentSceneName;
+
+    private void Awake()
+    {
+        string currentSceneName = SceneManager.GetActiveScene().name;
+    }
+
     // Loads Scenes
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    // Restarts level
+    public void restartLevel()
+    {
+        SceneManager.LoadScene(currentSceneName);
+    }
+
+    // Exits Game
+    public void Exit()
+    {
+        Application.Quit();
     }
 
 }
